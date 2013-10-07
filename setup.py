cb628e8528a1fe50754888836e8c88f941ce0ba1
Greg Lamp <lamp.greg@gmail.com>
2013-10-07 10:38:06 -0400

    basic setup.py file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+required = []
+
+setup(
+    name="yagg",
+    version="0.1.0",
+    author="Greg Lamp",
+    author_email="greg@yhathq.com",
+    url="https://github.com/yhat/yagg/",
+    license=open("LICENSE").read(),
+    packages=find_packages(),
+    package_dir={"ggplot": "ggplot"},
+    #package_data={"pandasql": ["data/*.csv"]},
+    description="ggplot for python",
+    long_description=open("README.md").read(),
+    install_requires=required,
+)
+
