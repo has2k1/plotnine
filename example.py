e7e3308fb35256af3abbb7f4935d09b681d09eab
Greg Lamp <lamp.greg@gmail.com>
2013-10-07 10:36:25 -0400

    changed to proper package structure

--- example.py
+++ example.py
@@ -16,7 +16,7 @@ df['y'] = np.sin(df.y)
 
 gg = ggplot(aes(x="x", y="z", color="cat", alpha=0.2), data=df)
 gg = ggplot(aes(x="x", color="c"), data=pd.DataFrame({"x": np.random.normal(0, 1, 10000), "c": ["blue" if i%2==0 else "red" for i in range(10000)]}))
-print gg + geom_density() + xlab("Density chart")
+print gg + geom_density() + xlab("x label") + ylab("y label")
 gg = ggplot(aes(x="x", y="y", shape="cat2", color="cat"), data=df)
 print gg + geom_point() + facet_wrap(x="cat", y="cat2") 
 print gg + geom_point() + facet_wrap(y="cat2") + ggtitle("My Single Facet") 
---
+++ ggplot/__init__.py
@@ -0,0 +1,1 @@
+from .ggplot import *
