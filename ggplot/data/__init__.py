c9b0bb0802ce101954f3d6e5e9d7ce9f90909ee7
Greg Lamp <lamp.greg@gmail.com>
2013-10-09 15:41:40 -0400

    adding meats to file

--- ggplot/data/__init__.py
+++ ggplot/data/__init__.py
@@ -6,4 +6,5 @@ _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 diamonds = pd.read_csv(os.path.join(_ROOT, "diamonds.csv"))
 mtcars = pd.read_csv(os.path.join(_ROOT, "mtcars.csv"))
+meat = pd.read_csv(os.path.join(_ROOT, "meat.csv"), parse_dates=[0]))
 
