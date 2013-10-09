a747517aebe26c6d120aa28b3e8a9ec6b02f5cd9
Greg Lamp <lamp.greg@gmail.com>
2013-10-09 18:08:42 -0400

    updated changelog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ required = []
 
 setup(
     name="ggplot",
-    version="0.1.5",
+    version="0.1.6",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/ggplot/",
