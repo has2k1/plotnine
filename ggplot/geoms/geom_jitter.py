3370b963fe24e82830d98500681fb18f766fbed2
Greg Lamp <lamp.greg@gmail.com>
2013-10-10 17:50:21 -0400

    fixed bug w/ jitter

--- ggplot/geoms/geom_jitter.py
+++ ggplot/geoms/geom_jitter.py
@@ -1,18 +1,21 @@
 from copy import deepcopy
 from geom import geom
 import numpy as np
+import pandas as pd
 
 class geom_jitter(geom):
     VALID_AES = ['jitter']
 
     def __radd__(self, gg):
         gg = deepcopy(gg)
-        x = gg.data['x']
-        y = gg.data['y']
+        xcol = gg.aesthetics.get("x")
+        ycol = gg.aesthetics.get("y")
+        x = gg.data[xcol]
+        y = gg.data[ycol]
         x = x * np.random.uniform(.9, 1.1, len(x))
         y = y * np.random.uniform(.9, 1.1, len(y))
-        gg.data['x'] = x
-        gg.data['y'] = y
+        gg.data[xcol] = x
+        gg.data[ycol] = y
         return gg
 
     def plot_layer(self, layer):
