c1135079f83f75df0d6bb4b891a383f4022ce575
Austin Ogilvie <austinogilvie@gmail.com>
2013-10-12 15:59:25 -0400

    scale x date start

--- ggplot/scales/__init__.py
+++ ggplot/scales/__init__.py
@@ -2,4 +2,5 @@ from .scale_colour_gradient import scale_colour_gradient
 from .scale_colour_gradient import scale_colour_gradient as scale_color_gradient
 from .scale_colour_gradient import scale_colour_gradient as scale_colour_gradient2
 from .scale_x_continuous import scale_x_continuous
+from .scale_x_date import scale_x_date
 from .scale_y_continuous import scale_y_continuous
