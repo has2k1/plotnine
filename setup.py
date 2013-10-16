d58c0aaa0b9b253f16000b8a1fb13b35d8ff6cb3
Greg <greg@yhathq.com>
2013-10-16 15:19:00 -0400

    inc minor version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ required = []
 
 setup(
     name="ggplot",
-    version="0.2.7",
+    version="0.2.8",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/ggplot/",
