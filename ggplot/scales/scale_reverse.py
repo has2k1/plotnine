d80b169a76a72e89cb69c34f1c199f5d418adb86
Greg <greg@yhathq.com>
2013-10-17 14:58:53 -0400

    fixed spacing

--- ggplot/scales/scale_reverse.py
+++ ggplot/scales/scale_reverse.py
@@ -3,14 +3,15 @@ from copy import deepcopy
 
 
 class scale_y_reverse(scale):
-        def __radd__(self, gg):
-            gg = deepcopy(gg)
-            gg.scale_y_reverse = True
-            return gg
+
+    def __radd__(self, gg):
+        gg = deepcopy(gg)
+        gg.scale_y_reverse = True
+        return gg
 
 
 class scale_x_reverse(scale):
-        def __radd__(self, gg):
-            gg = deepcopy(gg)
-            gg.scale_x_reverse = True
-            return gg
+    def __radd__(self, gg):
+        gg = deepcopy(gg)
+        gg.scale_x_reverse = True
+        return gg
