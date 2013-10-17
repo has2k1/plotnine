34b2385d6a3bb7acdbcd3f894d30dfbc734bd52e
Yaroslav Halchenko <debian@onerussian.com>
2013-10-17 12:19:41 -0400

    ENH: allow to set matplotlib backend from env MATPLOTLIB_BACKEND

--- ggplot/__init__.py
+++ ggplot/__init__.py
@@ -1,2 +1,15 @@
+# For testing purposes we might need to set mpl backend before any
+# other import of matplotlib.
+def _set_mpl_backend():
+    import os
+    import matplotlib as mpl
+
+    env_backend = os.environ.get('MATPLOTLIB_BACKEND')
+    if env_backend:
+        # we were instructed
+        mpl.use(env_backend)
+
+_set_mpl_backend()
+
 from .ggplot import *
 from .exampledata import *
