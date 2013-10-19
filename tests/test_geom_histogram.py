588230038b5ab6f5d6657b8158f74fead7445ab6
Tyler Funnell <tyler.funnell@gmail.com>
2013-10-18 17:29:16 -0700

    fixed test_geom_histogram to test geom_histogram

--- tests/test_geom_histogram.py
+++ tests/test_geom_histogram.py
@@ -4,7 +4,7 @@ from ggplot import *
 class TestGeomHistogram(unittest.TestCase):
 
     def test_geom_histogram(self):
-        gghist = ggplot(meat, aes(x='veal', y='beef')) + geom_point()
+        gghist = ggplot(meat, aes(x='veal', y='beef')) + geom_histogram()
         self.assertTrue(type(gghist) == ggplot)
         print gghist
 
