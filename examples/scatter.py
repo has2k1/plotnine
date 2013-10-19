5ded9c2ec767441b1668a4fb428d2457b55460ce
stonebig <stonebig>
2013-10-19 12:33:55 +0200

    make examples compatible with python3

--- examples/scatter.py
+++ examples/scatter.py
@@ -1,7 +1,7 @@
 from ggplot import *
 
 
-print ggplot(aes(x='date', y='beef'), data=meat) + \
-    geom_point()
+print (ggplot(aes(x='date', y='beef'), data=meat) + \
+    geom_point())
 
 plt.show(1)
