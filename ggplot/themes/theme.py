001d448818e9b084ba4fba4114eac49a73328adf
Jan Schulz <jasc@gmx.net>
2013-10-21 16:13:15 +0200

    Make themes work with a contextmanager
    Adding themes was not sideeffect free, meaning that after you added
    (or even instantiated a theme object) a theme all following plots
    (even direct matplotlib plots) had their defaults altered.
    
    This patch changes this so that themes add their rcParams to the
    ggplot object during __radd__() [and not directly to
    matplotlib.rcParams during __init__()] and during plotting a
    matplotlib.rc_context() contextmanager is used with this rcParams.

--- ggplot/themes/theme.py
+++ ggplot/themes/theme.py
@@ -1,12 +1,6 @@
-import matplotlib as mpl
-from theme_gray import theme_gray
+from copy import deepcopy
 
-class theme(theme_gray):
-    def __init__(self, params):
-        # Load the default gray theme then customize on top of it
-        super(theme_gray, self).__init__()
-        for param, value in params.items():
-            mpl.rcParams[param] = str(value)
-
+class theme(object):
     def __radd__(self, gg):
+        gg = deepcopy(gg)
         return gg
--- ggplot/themes/theme_bw.py
+++ ggplot/themes/theme_bw.py
@@ -1,11 +1,12 @@
-import matplotlib as mpl
 from .theme_gray import theme_gray
-
 
 class theme_bw(theme_gray):
     """
     White background w/ black gridlines
     """
-    def __init__(self):
-        super(theme_gray, self).__init__()
-        mpl.rcParams['axes.facecolor'] = 'white'
+   
+    def __radd__(self, gg):
+        # no need to copy as super will do it...
+        gg = super(theme_bw, self).__radd__(gg)
+        gg.rcParams['axes.facecolor'] = 'white'        
+        return gg
