1115761898abb9a773e125e2f22426afc4cbcfb3
Tyler Funnell <tyler.funnell@gmail.com>
2013-10-22 13:40:24 -0700

    adding binwidth to geom_histogram with default range/30

--- ggplot/geoms/geom_histogram.py
+++ ggplot/geoms/geom_histogram.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
+import sys
 from copy import deepcopy
 from .geom import geom
 
 
 class geom_histogram(geom):
-    VALID_AES = ['x', 'color', 'alpha', 'label']
+    VALID_AES = ['x', 'color', 'alpha', 'label', 'binwidth']
 
     def plot_layer(self, layer):
         layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
         layer.update(self.manual_aes)
+        if 'binwidth' in layer:
+            binwidth = layer.pop('binwidth')
+            try:
+                binwidth = float(binwidth)
+                bottom = plt.np.nanmin(layer['x'])
+                top = plt.np.nanmax(layer['x'])
+                layer['bins'] = plt.np.arange(bottom, top + binwidth, binwidth)
+            except:
+                pass
+        if 'bins' not in layer:
+            layer['bins'] = 30
+            sys.stderr.write("binwidth defaulted to range/30. " +
+                             "Use 'binwidth = x' to adjust this.\n")
+                
         plt.hist(**layer)
