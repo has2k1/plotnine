54be76ab7dcabbd106721e6f69c785ef32e3d29b
Jan Schulz <jasc@gmx.net>
2013-10-25 23:06:15 +0200

    Initial version of testing framework
    The testing framework is mostly a copy of the matplotlib framework
    and uses most of the testing facility of matplotlib.
    
    To write tests see the examples in  ggplot/tests/ or matplotlib/tests
    and the matplotlib testing documentation:
    http://matplotlib.org/devel/testing.html#testing
    Add new tests to default_test_modules in ggplot.tests.__init__.py
    
    To run the tests:
    cd temp
    python -c "import ggplot.tests;ggplot.tests.test()"
    OR
    python tests.py -v -d

--- ggplot/tests/__init__.py
+++ ggplot/tests/__init__.py
@@ -1,5 +1,43 @@
 import matplotlib as mpl
+    
+default_test_modules = [
+    'ggplot.tests.test_reverse',
+    ]
 
-def setup():
-    mpl.use('Agg', warn=False) # use Agg backend for these tests
-    mpl.interactive(False)
+
+# Testing framework shamelessly stolen from matplotlib...
+# This uses the matplotlib facilities...
+def test(verbosity=1):
+    """run the ggplot test suite"""
+    old_backend = mpl.rcParams['backend']
+    try:
+        mpl.use('agg')
+        import nose
+        import nose.plugins.builtin
+        from matplotlib.testing.noseclasses import KnownFailure
+        from nose.plugins.manager import PluginManager
+        from nose.plugins import multiprocess
+
+        # store the old values before overriding
+        plugins = []
+        plugins.append( KnownFailure() )
+        plugins.extend( [plugin() for plugin in nose.plugins.builtin.plugins] )
+
+        manager = PluginManager(plugins=plugins)
+        config = nose.config.Config(verbosity=verbosity, plugins=manager)
+
+        # Nose doesn't automatically instantiate all of the plugins in the
+        # child processes, so we have to provide the multiprocess plugin with
+        # a list.
+        multiprocess._instantiate_plugins = [KnownFailure]
+
+        success = nose.run( defaultTest=default_test_modules,
+                            config=config,
+                            )
+    finally:
+        if old_backend.lower() != 'agg':
+            mpl.use(old_backend)
+
+    return success
+
+test.__test__ = False # nose: this function is not a test
