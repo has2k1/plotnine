a4232c39a17275380ea988cd728a36c1a42869e2
Dan Blanchard <dblanchard@ets.org>
2013-10-28 13:46:18 -0400

    Update README.rst to contain same content as README.md and delete the latter

--- tests/test_geom_histogram.py
+++ tests/test_geom_histogram.py
@@ -1,3 +1,5 @@
+from __future__ import print_function, unicode_literals
+
 import unittest
 from ggplot import *
 
@@ -6,7 +8,7 @@ class TestGeomHistogram(unittest.TestCase):
     def test_geom_histogram(self):
         gghist = ggplot(meat, aes(x='veal')) + geom_histogram()
         self.assertTrue(type(gghist) == ggplot)
-        print gghist
+        print(gghist)
 
     def test_binwidth(self):
         gghist = ggplot(meat, aes(x='veal')) + geom_histogram(binwidth=10)
