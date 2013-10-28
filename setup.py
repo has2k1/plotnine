7d51a2055e260ccdf50b6e9e208bf9e6c9ce1df7
Dan Blanchard <dblanchard@ets.org>
2013-10-28 10:46:05 -0400

    Add support for Travis and Coveralls, and added requirements into setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
-from distutils.core import setup
-from setuptools import find_packages
 import os
-
-
-required = ["pandas"]
+from setuptools import find_packages, setup
 
 def extract_version():
     """
@@ -43,6 +39,19 @@ setup(
     description="ggplot for python",
     # run pandoc --from=markdown --to=rst --output=README.rst README.md
     long_description=open("README.rst").read(),
-    install_requires=required,
-)
-
+    install_requires=["pandas", "matplotlib", "scipy", "statsmodels",
+                      "patsy"],
+    classifiers=['Intended Audience :: Science/Research',
+                 'Intended Audience :: Developers',
+                 'Programming Language :: Python',
+                 'Topic :: Software Development',
+                 'Topic :: Scientific/Engineering',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: Unix',
+                 'Operating System :: MacOS',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3'],
+    zip_safe=False)
