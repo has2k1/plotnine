0228e038d666c4d433291da5aea050db1e6c7062
kalaidin <pavel.kalaidin@gmail.com>
2013-11-15 17:33:37 +0400

    Add grouping for geom_line.

--- ggplot/geoms/geom_line.py
+++ ggplot/geoms/geom_line.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+from itertools import groupby
+from operator import itemgetter
 from copy import deepcopy
 from .geom import geom
 
+
 class geom_line(geom):
-    VALID_AES = ['x', 'y', 'color', 'alpha', 'linestyle', 'label', 'size']
+    VALID_AES = ['x', 'y', 'color', 'alpha', 'linestyle', 'label', 'size', 'group']
     def plot_layer(self, layer):
         layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
         layer.update(self.manual_aes)
@@ -14,8 +17,12 @@ class geom_line(geom):
         if 'size' in layer:
             layer['markersize'] = layer['size']
             del layer['size']
-
         if 'linestyle' in layer and 'color' not in layer:
             layer['color'] = 'k'
-
-        plt.plot(x, y, **layer)
+        if 'group' not in layer:
+            plt.plot(x, y, **layer)
+        else:
+            g = layer.pop('group')
+            for k, v in groupby(sorted(zip(x, y, g), key=itemgetter(2)), key=itemgetter(2)):
+                x_g, y_g, _ = zip(*v) 
+                plt.plot(x_g, y_g, **layer)
