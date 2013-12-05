da35a82e459e8b034d9d86de380049dd01e4e7c8
Greg <greg@yhatHQ.com>
2013-12-05 13:15:21 -0500

    inc minor version for patch

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def get_package_data():
 
 setup(
     name="ggplot",
-    version="0.4.1",
+    version="0.4.2",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/ggplot/",
