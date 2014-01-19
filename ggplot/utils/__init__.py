36e32c46e6222f0da5c009b370717a303a7008e9
Jan Schulz <jasc@gmx.net>
2014-01-19 23:28:40 +0100

    Workaround for matplotlib 1.1.1 and missing rc_context
    Monkey patch the rc_context into this old version on import

--- ggplot/utils/__init__.py
+++ ggplot/utils/__init__.py
@@ -3,3 +3,15 @@ from .date_breaks import date_breaks
 from .date_format import date_format
 
 __ALL__ = ["ggsave", "date_breaks", "date_format"]
+
+class _rc_context(object):
+    def __init__(self, fname=None):
+        self.fname = fname
+    def __enter__(self):
+        import matplotlib as mpl
+        self._rcparams = mpl.rcParams.copy()
+        if self.fname:
+            mpl.rcfile(self.fname)
+    def __exit__(self, type, value, tb):
+        import matplotlib as mpl
+        mpl.rcParams.update(self._rcparams)
