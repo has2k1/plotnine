adfe36f8777b560ad41e1958cca500cfc5076eaa
Jan Schulz <jasc@gmx.net>
2014-01-21 20:24:49 +0100

    FIX: old usage of facet_grid in old manual tests

--- tests/test_facets.py
+++ tests/test_facets.py
@@ -10,7 +10,5 @@ print(p + geom_point() + facet_wrap("cut", ncol=3))
 print(p + geom_point() + facet_wrap("cut"))
 
 p = ggplot(aes(x='x', y='y'), data=diamonds)
-print(p + geom_point() + facet_grid("cut", "clarity", nrow=5, ncol=8))
-print(p + geom_point() + facet_grid("cut", "clarity", nrow=5))
-print(p + geom_point() + facet_grid("cut", "clarity",  ncol=5))
+print(p + geom_point() + facet_grid("cut", "clarity"))
 print(p + geom_point() + facet_grid("cut", "clarity", scales="free"))
