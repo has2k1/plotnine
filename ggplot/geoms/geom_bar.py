0a1ba2df0eac2b4fecf79726e745089c75b3292f
Tyler Funnell <tyler.funnell@gmail.com>
2014-01-31 15:51:20 -0800

    changed bar's defaults to be thicker and black

--- ggplot/geoms/geom_bar.py
+++ ggplot/geoms/geom_bar.py
@@ -12,6 +12,7 @@ class geom_bar(geom):
     def plot_layer(self, layer):
         layer = {k: v for k, v in layer.items() if k in self.VALID_AES}
         layer.update(self.manual_aes)
+
         x = layer.pop('x')
         if 'weight' not in layer:
             counts = pd.value_counts(x)
@@ -30,11 +31,24 @@ class geom_bar(geom):
                 ts = ts.fillna(0)
                 weights = ts.values.tolist()
                 labels = ts.index.to_pydatetime().tolist()
+
         indentation = np.arange(len(labels)) + 0.2
-        width = 0.35
+        width = 0.9
         idx = np.argsort(labels)
         labels, weights = np.array(labels)[idx], np.array(weights)[idx]
         labels = sorted(labels)
+
+        if 'color' in layer:
+            layer['edgecolor'] = layer['color']
+            del layer['color']
+        else:
+            layer['edgecolor'] = '#333333'
+
+        if 'fill' in layer:
+            layer['color'] = layer['fill']
+            del layer['fill']
+        else:
+            layer['color'] = '#333333'
 
         plt.bar(indentation, weights, width, **layer)
         plt.autoscale()
