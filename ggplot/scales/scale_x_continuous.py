6bb65c615b9f1e94e701c2e086b6b0542cbddc22
Greg <greg@yhatHQ.com>
2014-02-01 17:26:19 -0500

    fixing up docstring

--- ggplot/scales/scale_x_continuous.py
+++ ggplot/scales/scale_x_continuous.py
@@ -2,16 +2,22 @@ from .scale import scale
 from copy import deepcopy
 
 class scale_x_continuous(scale):
-    "scale x axis as continuous"
     """
-    breaks => list (maps to xbreaks)
-    labels => list (maps to xtick_labels)
+    Scale x axis as continuous values
 
-    example:
-        print ggplot(mtcars, aes('mpg', 'qsec')) + \
-            geom_point() + \
-            scale_x_continuous(breaks=[10,20,30],  \
-                labels=["horrible", "ok", "awesome"])
+    Parameters
+    ----------
+    breaks: list
+        maps to xbreaks
+    labels: list
+        maps to xtick_labels
+
+    Examples
+    --------
+    >>> print ggplot(mtcars, aes('mpg', 'qsec')) + \
+    ...     geom_point() + \
+    ...     scale_x_continuous(breaks=[10,20,30],  \
+    ...     labels=["horrible", "ok", "awesome"])
 
     """
     VALID_SCALES = ['name', 'limits', 'labels', 'breaks', 'trans']
