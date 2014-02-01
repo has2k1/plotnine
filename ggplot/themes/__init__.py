8f15684e7700782418ae52364e2ec124783366fa
Greg <greg@yhatHQ.com>
2014-02-01 16:31:00 -0500

    seaborn-esque theme

--- ggplot/themes/__init__.py
+++ ggplot/themes/__init__.py
@@ -3,5 +3,7 @@ from .theme_bw import theme_bw
 from .theme_gray import theme_gray
 from .theme_xkcd import theme_xkcd
 from .theme_matplotlib import theme_matplotlib
+from .theme_seaborn import theme_seaborn
 
-__ALL__ = ["theme_bw","theme_gray","theme_xkcd","theme_matplotlib"]
+__ALL__ = ["theme_bw","theme_gray","theme_xkcd","theme_matplotlib",
+           "theme_seaborn"]
--- ggplot/themes/theme.py
+++ ggplot/themes/theme.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 
 class theme(object):
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
     def __radd__(self, gg):
         gg = deepcopy(gg)
         gg.theme_applied = True
