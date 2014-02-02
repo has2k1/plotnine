92814d033c5ae4cee885c64044751093c073ee8e
Greg <greg@yhatHQ.com>
2014-02-02 13:16:21 -0500

    scale discrete

--- ggplot/scales/__init__.py
+++ ggplot/scales/__init__.py
@@ -7,8 +7,10 @@ from .scale_colour_manual import scale_colour_manual
 from .scale_facet import scale_facet_grid
 from .scale_facet import scale_facet_wrap
 from .scale_x_continuous import scale_x_continuous
+from .scale_x_discrete import scale_x_discrete
 from .scale_x_date import scale_x_date
 from .scale_y_continuous import scale_y_continuous
+from .scale_y_discrete import scale_y_discrete
 from .scale_reverse import scale_y_reverse, scale_x_reverse
 from .scale_log import scale_y_log, scale_x_log
 
