12e7d1994f16365c46aae5eb3e0ec3ae5e16f7d2
Greg <greg@yhatHQ.com>
2014-02-08 20:40:08 -0500

    fixing shit

--- ggplot/components/colors.py
+++ ggplot/components/colors.py
@@ -1,23 +1,30 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 import sys
 import numpy as np
 from matplotlib.colors import rgb2hex
 import ggplot.utils.six as six
 
-COLORS = [
-    "#000000",
-    "#E69F00",
-    "#56B4E9",
-    "#009E73",
-    "#F0E442",
-    "#0072B2",
-    "#D55E00",
-    "#CC79A7"
-]
 
+def to_hex(r, g, b):
+    return rgb2hex((r/255., g/255., b/255.))
 
-def color_gen(colors=COLORS):
+hue = 65
+COLORS = []
+for i in range(max(hue, 90), 256):
+    COLORS.append(to_hex(255, i, hue))
+for i in range(hue, 256):
+    COLORS.append(to_hex(255-i, 255, hue))
+for i in range(hue, 256):
+    COLORS.append(to_hex(hue, 255, i))
+for i in range(hue, 256):
+    COLORS.append(to_hex(hue, 255-i, 255))
+for i in range(hue, 256):
+    COLORS.append(to_hex(i, hue, 255))
+for i in range(hue, 256):
+    COLORS.append(to_hex(255, hue, 255-i))
+for i in range(hue, max(hue, 90)):
+    COLORS.append(to_hex(255, i, hue))
+
+def color_gen(n_colors, colors=None):
     """
     Generator that will infinitely produce colors when asked politely
     TODO: This needs to be updated with better colors, but it will do for now.
@@ -26,8 +33,12 @@ def color_gen(colors=COLORS):
         colors - a list of colors. can be hex or actual names
     """
     while True:
-        for color in colors:
-            yield color
+        if colors is None:
+            for idx in range(0, len(COLORS), len(COLORS)/n_colors):
+                yield COLORS[idx]
+        else:
+            for color in colors:
+                yield color
 
 
 def assign_colors(data, aes, gg):
@@ -78,9 +89,9 @@ def assign_colors(data, aes, gg):
         else:
             possible_colors = np.unique(data[color_col])
             if gg.manual_color_list:
-                color = color_gen(gg.manual_color_list)
+                color = color_gen(len(possible_colors), gg.manual_color_list)
             else:
-                color = color_gen()
+                color = color_gen(len(possible_colors))
             color_mapping = {value: six.next(color) for value in possible_colors}
             data["color_mapping"] = data[color_col].apply(lambda x: color_mapping[x])
             gg.add_to_legend("color", {v: k for k, v in color_mapping.items()})
--- ggplot/themes/theme_bw.py
+++ ggplot/themes/theme_bw.py
@@ -1,5 +1,3 @@
-from __future__ import (absolute_import, division, print_function,
-                        unicode_literals)
 from .theme_gray import theme_gray
 
 class theme_bw(theme_gray):
