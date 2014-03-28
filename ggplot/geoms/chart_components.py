b28e9e26eaa8e212581fde1741b502cc85a83789
cosinequanon <sgoder@deem.com>
2014-03-27 20:28:32 -0700

    Add checks to chart components on init.
    Check for components that should never
    be None and check for components that
    should be of a numeric type.

--- ggplot/geoms/chart_components.py
+++ ggplot/geoms/chart_components.py
@@ -1,9 +1,14 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 from copy import deepcopy
+from ggplot.utils.exceptions import GgplotError
+
 
 class ggtitle(object):
     def __init__(self, title):
+        if title is None:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "cannot be None")
         self.title = title
 
     def __radd__(self, gg):
@@ -11,17 +16,17 @@ class ggtitle(object):
         gg.title = self.title
         return gg
 
-class xlab(object):
-    def __init__(self, xlab):
-        self.xlab = xlab
-
-    def __radd__(self, gg):
-        gg = deepcopy(gg)
-        gg.xlab = self.xlab
-        return gg
 
 class xlim(object):
     def __init__(self, low, high):
+        if low is None or high is None:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "cannot be None")
+        try:
+            _ = high - low
+        except TypeError:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "must be of a numeric type")
         self.low, self.high = low, high
 
     def __radd__(self, gg):
@@ -29,8 +34,17 @@ class xlim(object):
         gg.xlimits = [self.low, self.high]
         return gg
 
+
 class ylim(object):
     def __init__(self, low, high):
+        if low is None or high is None:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "cannot be None")
+        try:
+            _ = high - low
+        except TypeError:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "must be of a numeric type")
         self.low, self.high = low, high
 
     def __radd__(self, gg):
@@ -38,14 +52,32 @@ class ylim(object):
         gg.ylimits = [self.low, self.high]
         return gg
 
+
+class xlab(object):
+    def __init__(self, xlab):
+        if xlab is None:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "cannot be None")
+        self.xlab = xlab
+
+    def __radd__(self, gg):
+        gg = deepcopy(gg)
+        gg.xlab = self.xlab
+        return gg
+
+
 class ylab(object):
     def __init__(self, ylab):
+        if ylab is None:
+            raise GgplotError("Arguments to", self.__class__.__name__,
+                              "cannot be None")
         self.ylab = ylab
 
     def __radd__(self, gg):
         gg = deepcopy(gg)
         gg.ylab = self.ylab
         return gg
+
 
 class labs(object):
     def __init__(self, x=None, y=None, title=None):
