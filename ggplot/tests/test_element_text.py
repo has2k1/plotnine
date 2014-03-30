e20d7c659e6b38e553ccef302e5438fd718743cc
George A. Dowding <pyrios@gmail.com>
2014-03-30 19:57:23 -0400

    Fix python3 test fail

--- ggplot/tests/test_element_text.py
+++ ggplot/tests/test_element_text.py
@@ -13,30 +13,30 @@ gg = ggplot(aes(x='date', y='beef'), data=meat) + \
 @image_comparison(["all_text"], extensions=["png"], tol=0)
 def test_element_text1():
     # Text element properties that can be configured with rcParams.
-    print gg + theme(text=element_text(family="serif", face="bold.italic",
-                                       size=20, color="red", angle=45))
+    print(gg + theme(text=element_text(family="serif", face="bold.italic",
+                                       size=20, color="red", angle=45)))
 
 @image_comparison(["axis_text"], extensions=["png"], tol=0)
 def test_element_text2():
     # Text element properties that can be configured with rcParams.
-    print gg + \
-        theme(text=element_text(family="serif", face="bold.italic",
-                                size=20, color="red")) + \
-        theme(axis_text=element_text(color="green", angle=45))
+    print(gg +
+          theme(text=element_text(family="serif", face="bold.italic",
+                                  size=20, color="red")) +
+          theme(axis_text=element_text(color="green", angle=45)))
 
 @image_comparison(["axis_title"], extensions=["png"], tol=0)
 def test_element_text3():
     # Text element properties that can be configured with rcParams.
-    print gg + \
-        theme(text=element_text(family="serif", face="bold.italic",
-                                size=20, color="red")) + \
-        theme(axis_title=element_text(color="purple"))
+    print (gg +
+           theme(text=element_text(family="serif", face="bold.italic",
+                                   size=20, color="red")) +
+           theme(axis_title=element_text(color="purple")))
 
 @image_comparison(["axis_title_text"], extensions=["png"], tol=0)
 def test_element_text4():
     # Text element properties that can be configured with rcParams.
-    print gg + \
-        theme(text=element_text(family="serif", face="bold.italic",
-                                size=20, color="red")) + \
-        theme(axis_title=element_text(color="blue")) + \
-        theme(axis_text_y=element_text(color="green"))
+    print(gg +
+          theme(text=element_text(family="serif", face="bold.italic",
+                                  size=20, color="red")) +
+          theme(axis_title=element_text(color="blue")) +
+          theme(axis_text_y=element_text(color="green")))
