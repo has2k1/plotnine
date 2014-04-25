b8f8bbb6e8f5480174c4c49445bc0e7d952f3946
ericchiang <eric.chiang.m@gmail.com>
2014-04-25 14:46:10 -0400

    geom_abline fixed

--- ggplot/geoms/geom_abline.py
+++ ggplot/geoms/geom_abline.py
@@ -7,24 +7,30 @@ from .geom import geom
 
 class geom_abline(geom):
     DEFAULT_AES = {'color': 'black', 'linetype': 'solid', 'alpha': None, 'size': 1.0}
-    REQUIRED = {'x'} # TODO: Why is x needed.
+    REQUIRED = {} 
     DEFAULT_PARAMS = {'stat': 'abline', 'position': 'identity', 'slope': 1.0, 'intercept': 0.0, 'label': ''}
 
     _aes_renames = {'linetype': 'linestyle', 'size': 'linewidth'}
     _groups = {'alpha', 'color', 'linestyle', 'linewidth'}
 
     def _plot_unit(self, pinfo, ax):
-        x = pinfo.pop('x')
         slope = self.params['slope']
         intercept = self.params['intercept']
         pinfo['label'] = self.params['label']
-        if isinstance(x[0], Timestamp):
-            ax.set_autoscale_on(False)
-            ax.plot(ax.get_xlim(),ax.get_ylim())
-        else:
-            start, stop = np.max(x), np.min(x)
-            step = ((stop-start))  / 100.0
-            x_rng = np.arange(start, stop, step)
-            y_rng = x_rng * slope + intercept
-            ax.plot(x_rng, y_rng, **pinfo)
-
+        ax.set_autoscale_on(False)
+        # Get plot limits
+        xlim = ax.get_xlim()
+        ylim = ax.get_ylim()
+        # Determine line function
+        line_func = lambda x: x*slope + intercept
+        # Get "x" points within limit and map to "y" coordinates
+        x_points = np.linspace(min(xlim),max(xlim),100)
+        y_points = np.array(map(line_func,x_points))
+        # detemine which points are still within the limit of the plot
+        in_range = np.logical_and(y_points > min(ylim),
+                                  y_points < max(ylim))
+        # If some points are withinn the plot draw them on 
+        if np.sum(in_range) > 0:
+            x_points = x_points[in_range]
+            y_points = y_points[in_range]
+            ax.plot(x_points,y_points,**pinfo)
