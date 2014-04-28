2ca21f7d29015faf98d965392d48078f0d62c781
George A. Dowding <pyrios@gmail.com>
2014-04-27 20:53:01 -0400

    Remove serif. That styling is silently ignored by matplotlib if unavailable.

--- ggplot/tests/test_element_text.py
+++ ggplot/tests/test_element_text.py
@@ -11,25 +11,25 @@ df = DataFrame({"blahblahblah": linspace(999, 1111, 9),
 simple_gg = ggplot(aes(x="blahblahblah", y="yadayadayada"), data=df) + geom_line()
 
     
-@image_comparison(["all_text"], extensions=["png"], tol=10)
+@image_comparison(["all_text"], tol=10)
 def test_element_text1():
-    print(simple_gg + theme(text=element_text(family="serif", face="bold.italic",
+    print(simple_gg + theme(text=element_text(family="serif", face="bold",
                                        size=50, color="red", angle=45)))
 
-@image_comparison(["axis_text"], extensions=["png"], tol=10)
+@image_comparison(["axis_text"], tol=10)
 def test_element_text2():
     #print(simple_gg)
     print(simple_gg +
           theme(text=element_text(face="bold", size=50, color="red")) +
           theme(axis_text=element_text(color="green", angle=45)))
 
-@image_comparison(["axis_title"], extensions=["png"], tol=10)
+@image_comparison(["axis_title"], tol=10)
 def test_element_text3():
     print (simple_gg +
            theme(text=element_text(face="bold", color="red")) +
            theme(axis_title=element_text(color="purple", size=50)))
 
-@image_comparison(["axis_title_text"], extensions=["png"], tol=15)
+@image_comparison(["axis_title_text"], tol=15)
 def test_element_text4():
     print(simple_gg +
           theme(text=element_text(face="bold", color="red")) +
