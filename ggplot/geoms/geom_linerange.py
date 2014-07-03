fc75be5bd565fc805b6d220daed0762410bf5565
Matthias Kümmerer <matthias@matthias-k.org>
2014-07-03 14:53:37 +0200

    geom_linerange: Support for categorical x
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- ggplot/geoms/geom_linerange.py
+++ ggplot/geoms/geom_linerange.py
@@ -2,6 +2,8 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import matplotlib as mpl
 from .geom import geom
+from ggplot.utils import is_string
+from ggplot.utils import is_categorical
 import numpy as np
 
 class geom_linerange(geom):
@@ -16,6 +18,14 @@ class geom_linerange(geom):
     _units = {'alpha', 'color', 'linestyle'}# 'marker'}
 
     def _plot_unit(self, pinfo, ax):
+        # If x is categorical, calculate positions to plot
+        categorical = is_categorical(pinfo['x'])
+        if categorical:
+            x = pinfo.pop('x')
+            new_x = np.arange(len(x))
+            ax.set_xticks(new_x)
+            ax.set_xticklabels(x)
+            pinfo['x'] = new_x
 
         if 'linewidth' in pinfo and isinstance(pinfo['linewidth'], list):
             # ggplot also supports aes(size=...) but the current mathplotlib
@@ -27,22 +37,6 @@ class geom_linerange(geom):
                       "Use 'geom_line(size=x)' to set the size for the whole line.\n"
                 sys.stderr.write(msg)
                 self._warning_printed = True
-
-        #_abscent = {None: pinfo['color'], False: ''}
-        #try:
-        #    if pinfo['facecolor'] in _abscent:
-        #        pinfo['facecolor'] = _abscent[pinfo['facecolor']]
-        #except TypeError:
-        #    pass
-
-        # for some reason, scatter doesn't default to the same color styles
-        # as the axes.color_cycle
-        #f "color" not in pinfo and self.params['cmap'] is None:
-        #   pinfo["color"] = mpl.rcParams.get("axes.color_cycle", ["#333333"])[0]
-
-        #if self.params['position'] == 'jitter':
-        #    pinfo['x'] *= np.random.uniform(.9, 1.1, len(pinfo['x']))
-        #    pinfo['y'] *= np.random.uniform(.9, 1.1, len(pinfo['y']))
 
         x = pinfo.pop('x')
         x = np.vstack([x, x])
