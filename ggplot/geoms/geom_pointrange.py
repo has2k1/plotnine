0fded4d26ca8842bb57051117b2b55d4ee39057f
Matthias Kümmerer <matthias@matthias-k.org>
2014-07-10 11:34:01 +0200

    Docstring for geom_pointrange
    Signed-off-by: Matthias Kümmerer <matthias@matthias-k.org>

--- ggplot/geoms/geom_pointrange.py
+++ ggplot/geoms/geom_pointrange.py
@@ -10,6 +10,55 @@ import numpy as np
 
 
 class geom_pointrange(geom):
+    """Plot intervals represented by vertical lines with a point in each interval
+
+     Parameters
+     ---------
+
+     x
+         x values of data
+     y
+         y value of the point for each x
+     ymin
+         lower end of the interval for each x
+     ymax
+         upper end of the interval for each x
+     alpha : float
+         alpha value, defaults to 1
+     color : string
+         line color, defaults to 'black'
+     fill : string
+         Fill type for the points, defaults to 'None'
+     linetype : string
+         line type, defaults to 'solid'
+     shape : string
+         shape of the points, defaults to 'o' (i.e. circles)
+     size : string
+         width of the line and size of the point, defaults to 2
+
+     Examples
+     --------
+
+     .. plot::
+         :include-source:
+
+         import numpy as np
+         import pandas as pd
+         from ggplot import *
+
+         np.random.seed(42)
+         x = np.linspace(0.5, 9.5, num=10)
+         y = np.random.randn(10)
+         ymin = y - np.random.uniform(0,1, size=10)
+         ymax = y + np.random.uniform(0,1, size=10)
+
+         data = pd.DataFrame({'x': x, 'y': y, 'ymin': ymin, 'ymax': ymax})
+
+         ggplot(aes(x='x', y='y', ymin='ymin', ymax='ymax'), data) \
+             + geom_pointrange()
+
+    """
+
     DEFAULT_AES = {'alpha': 1, 'color': 'black', 'fill': None,
                    'linetype': 'solid',
                    'shape': 'o',
