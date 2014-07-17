331f0c8ce1b7f1d99495600a5405d9126a56a66e
Greg Lamp <lamp.greg@gmail.com>
2014-07-17 09:17:17 -0400

    adding in movies dataset

--- ggplot/exampledata/__init__.py
+++ ggplot/exampledata/__init__.py
@@ -4,11 +4,12 @@ import pandas as pd
 import matplotlib.image as mpimg
 import os
 
-__all__ = ["diamonds","mtcars","meat","pageviews"]
+__all__ = ["diamonds","mtcars","meat","pageviews","movies"]
 __all__ = [str(u) for u in __all__]
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 diamonds = pd.read_csv(os.path.join(_ROOT, "diamonds.csv"))
 mtcars = pd.read_csv(os.path.join(_ROOT, "mtcars.csv"))
 meat = pd.read_csv(os.path.join(_ROOT, "meat.csv"), parse_dates=[0])
+movies = pd.read_csv(os.path.join(_ROOT, "movies.csv"))
 pageviews = pd.read_csv(os.path.join(_ROOT, "pageviews.csv"), parse_dates=[0])
