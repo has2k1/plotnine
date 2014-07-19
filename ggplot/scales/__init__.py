58a0b5c0ea45aad6de4d9ecd7f32b134c02a7af5
Jan Schulz <jasc@gmx.net>
2014-07-19 17:33:34 +0200

    Fix: export all scales

--- ggplot/scales/__init__.py
+++ ggplot/scales/__init__.py
@@ -20,11 +20,15 @@ from .scale_log import scale_x_log, scale_y_log
 from .scale_log import scale_x_log as scale_x_log10
 from .scale_log import scale_y_log as scale_y_log10
 
-__all__ = ['scale_color_brewer', 'scale_colour_brewer',
-           'scale_color_gradient', 'scale_colour_gradient',
-           'scale_colour_gradient2', 'scale_colour_manual', 'scale_facet', 
-           'scale_facet_grid', 'scale_facet_wrap', 'scale_reverse', 
-           'scale_x_continuous', 'scale_x_date', 'scale_x_reverse', 
-           'scale_y_continuous', 'scale_y_reverse', 'scale_x_log',
-           'scale_y_log', 'scale_x_log10', 'scale_y_log10']
+__all__ = ['scale_colour_brewer', 'scale_color_brewer',
+           'scale_colour_gradient', 'scale_color_gradient',
+           'scale_colour_gradient2', 'scale_color_gradient2', 
+           'scale_colour_manual', 'scale_color_manual',
+           'scale_facet_grid', 'scale_facet_wrap', 
+           'scale_x_continuous', 'scale_y_continuous',
+           'scale_x_discrete', 'scale_y_discrete',
+           'scale_x_reverse','scale_y_reverse', 
+           'scale_x_date', # scale_y_date is missing
+           'scale_x_log', 'scale_y_log',
+           'scale_x_log10', 'scale_y_log10']
 __all__ = [str(u) for u in __all__]
