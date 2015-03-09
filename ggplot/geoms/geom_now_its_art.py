5384ac2a231826bf858972d86ec088907520b227
Hassan Kibirige <has2k1@gmail.com>
2015-03-09 04:01:49 -0500

    Update geom_text, geom_now_its_art
    - For geom_text, ggplot infrastructure now takes care of
      the axis dimensions

--- ggplot/geoms/geom_now_its_art.py
+++ ggplot/geoms/geom_now_its_art.py
@@ -1,8 +1,10 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
-import matplotlib.image as mpimg
-from .geom import geom
+
 import os
+import matplotlib.image as mpimg
+
+from .geom import geom
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -11,7 +13,8 @@ class geom_now_its_art(geom):
     DEFAULT_AES = {'alpha': 0.5}
     DEFAULT_PARAMS = {'stat': 'identity', 'position': 'identity'}
 
-    def _plot_unit(self, pinfo, ax):
+    @staticmethod
+    def draw(pinfo, scales, ax, **kwargs):
         img = mpimg.imread(os.path.join(_ROOT, 'bird.png'))
         ax.imshow(img, alpha=pinfo['alpha'])
         print ("Put a bird on it!")
--- ggplot/geoms/geom_text.py
+++ ggplot/geoms/geom_text.py
@@ -1,19 +1,23 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import numpy as np
+
 from .geom import geom
+
 
 class geom_text(geom):
     DEFAULT_AES = {'alpha': None, 'angle': 0, 'color': 'black', 'family': None,
                    'fontface': 1, 'hjust': None, 'size': 12, 'vjust': None,
                    'lineheight': 1.2}
-    REQUIRED_AES = {'label','x','y'}
-    DEFAULT_PARAMS = {'stat': 'identity', 'position': 'identity', 'parse': False}
+    REQUIRED_AES = {'label', 'x', 'y'}
+    DEFAULT_PARAMS = {'stat': 'identity', 'position': 'identity',
+                      'parse': False}
 
     _aes_renames = {'angle': 'rotation', 'lineheight': 'linespacing'}
     _units = {'alpha', 'color', 'family', 'size'}
 
-    def _plot_unit(self, pinfo, ax):
+    @staticmethod
+    def draw(pinfo, scales, ax, **kwargs):
         x = pinfo.pop('x')
         y = pinfo.pop('y')
         label = pinfo.pop('label')
@@ -23,38 +27,6 @@ class geom_text(geom):
         # "plain", "bold", "italic", "oblique", and "bold.italic"
         pinfo.pop('fontface')
 
-        # before taking max and min make sure x is not empty
-        if len(x) == 0:
-            return
-
-        # plt.text does not resize axes, must do manually
-        xmax = max(x)
-        xmin = min(x)
-        ymax = max(y)
-        ymin = min(y)
-
-        margin = 0.1
-        xmargin = (xmax - xmin) * margin
-        ymargin = (ymax - ymin) * margin
-        xmax = xmax + xmargin
-        xmin = xmin - xmargin
-        ymax = ymax + ymargin
-        ymin = ymin - ymargin
-
-        # Take current plotting dimension in account for the case that we
-        # work on a special dataframe just for this geom!
-        if not self.data is None:  # NOTE: not working??
-            cxmin, cxmax = ax.get_xlim()
-            cymin, cymax = ax.get_ylim()
-            # there is a problem if geom_text is the first plot, as
-            # then the dimension are 0-1 for all axis :-(
-            xmax = max(xmax, cxmax)
-            xmin = min(xmin, cxmin)
-            ymax = max(ymax, cymax)
-            ymin = min(ymin, cymin)
-
-        # TODO: Fix the defaults for this
-        # try out 0.5
         if pinfo['hjust'] is not None:
             x = (np.array(x) + pinfo['hjust']).tolist()
         else:
@@ -67,9 +39,5 @@ class geom_text(geom):
 
         del pinfo['hjust']
         del pinfo['vjust']
-        for x_g,y_g,s in zip(x,y,label):
-            ax.text(x_g,y_g,s,**pinfo)
-
-        # TODO: Find out why this isn't working as desired
-        # resize axes
-        ax.axis([xmin, xmax, ymin, ymax])
+        for x_g, y_g, s in zip(x, y, label):
+            ax.text(x_g, y_g, s, **pinfo)
