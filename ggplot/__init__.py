06df8dcb73e136280672413c78e0e3765f6bf2a3
Hassan Kibirige <has2k1@gmail.com>
2015-08-19 21:32:44 -0500

    qplot needs to be rewritten

--- ggplot/__init__.py
+++ ggplot/__init__.py
@@ -18,7 +18,7 @@ _set_mpl_backend()
 # version of ggplot.
 __version__ = '0.6.6'
 
-from .qplot import qplot
+# from .qplot import qplot
 from .ggplot import ggplot
 from .components import *
 from .coords import *
