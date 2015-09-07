dd8301eb80578b59d2148d4130e6dfcfbe6cf219
Hassan Kibirige <has2k1@gmail.com>
2015-09-07 02:37:22 -0500

    Make geom_text aware of position_nudge
    - `hjust` and `vjust` are turned into parameter due to MPL
      limitations.

--- ggplot/geoms/geom_text.py
+++ ggplot/geoms/geom_text.py
@@ -1,53 +1,68 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import numpy as np
 from matplotlib.text import Text
+from six.moves import zip
 
 from .geom import geom
-from ..utils import make_iterable_ntimes
+from ..utils import make_iterable_ntimes, to_rgba, suppress
+from ..positions import position_nudge
 
 
+# Note: hjust & vjust are parameters instead of aesthetics
+# due to a limitation imposed by MPL
+# see: https://github.com/matplotlib/matplotlib/pull/1181
 class geom_text(geom):
-    DEFAULT_AES = {'alpha': None, 'angle': 0, 'color': 'black', 'family': None,
-                   'fontface': 1, 'hjust': None, 'size': 12, 'vjust': None,
+    DEFAULT_AES = {'alpha': None, 'angle': 0, 'color': 'black',
+                   'family': None, 'fontface': 1, 'size': 12,
                    'lineheight': 1.2}
     REQUIRED_AES = {'label', 'x', 'y'}
     DEFAULT_PARAMS = {'stat': 'identity', 'position': 'identity',
-                      'parse': False}
+                      'parse': False, 'hjust': 'center',
+                      'vjust': 'center', 'nudge_x': 0, 'nudge_y': 0}
 
-    _units = {'alpha', 'color', 'family', 'size'}
+    def __init__(self, *args, **kwargs):
+        nudge_kwargs = {}
+        with suppress(KeyError):
+            nudge_kwargs['x'] = kwargs['nudge_x']
+        with suppress(KeyError):
+            nudge_kwargs['y'] = kwargs['nudge_y']
+        if nudge_kwargs:
+            kwargs['position'] = position_nudge(**nudge_kwargs)
+        geom.__init__(self, *args, **kwargs)
 
     @staticmethod
     def draw(pinfo, scales, coordinates, ax, **params):
         x = pinfo.pop('x')
         y = pinfo.pop('y')
-        # pinfo['label'] = [pinfo['label']]
+        color = to_rgba(pinfo['color'], pinfo['alpha'])
         # TODO: Deal with the fontface
         # from ggplot2
         # 1 = plain, 2 = bold, 3 = italic, 4 = bold italic
         # "plain", "bold", "italic", "oblique", and "bold.italic"
         pinfo.pop('fontface')
-        pinfo['horizontalalignment'] = 'center'
-        pinfo['verticalalignment'] = 'center'
 
-        if pinfo['hjust'] is not None:
-            x = (np.array(x) + pinfo['hjust']).tolist()
+        n = len(x)
+        if isinstance(color, tuple):
+            color = [color] * n
+        labels = make_iterable_ntimes(pinfo['label'], n)
+        lineheight = make_iterable_ntimes(pinfo['lineheight'], n)
+        size = make_iterable_ntimes(pinfo['size'], n)
+        family = make_iterable_ntimes(pinfo['family'], n)
+        angle = make_iterable_ntimes(pinfo['angle'], n)
 
-        if pinfo['vjust'] is not None:
-            y = (np.array(y) + pinfo['vjust']).tolist()
-
-        pinfo['label'] = make_iterable_ntimes(pinfo['label'], len(x))
-        for x_g, y_g, s in zip(x, y, pinfo['label']):
+        if params['parse']:
+            labels = ['${}$'.format(l) for l in labels]
+        items = (x, y, labels, color, family, size, lineheight, angle)
+        for x_g, y_g, s, col, fam, sz, lh, ang in zip(*items):
             ax.text(x_g, y_g, s,
-                    alpha=pinfo['alpha'],
-                    color=pinfo['color'],
-                    size=pinfo['size'],
-                    linespacing=pinfo['lineheight'],
-                    family=pinfo['family'],
-                    verticalalignment=pinfo['verticalalignment'],
-                    horizontalalignment=pinfo['horizontalalignment'],
-                    rotation=pinfo['angle'],
+                    color=col,
+                    family=fam,
+                    size=sz,
+                    linespacing=lh,
+                    verticalalignment=params['vjust'],
+                    horizontalalignment=params['hjust'],
+                    rotation=ang,
                     zorder=pinfo['zorder'])
 
     @staticmethod
