a765149f3c75c8ce8321bff59fe2b6de7a6d9ade
Hassan Kibirige <has2k1@gmail.com>
2015-10-07 18:47:28 -0500

    Fix geom_jitter width,height & position parameters

--- ggplot/geoms/geom_jitter.py
+++ ggplot/geoms/geom_jitter.py
@@ -8,21 +8,24 @@ from .geom_point import geom_point
 
 
 class geom_jitter(geom_point):
-    DEFAULT_PARAMS = {'stat': 'identity', 'position': 'jitter'}
+    DEFAULT_PARAMS = {'stat': 'identity', 'position': 'jitter',
+                      'width': None, 'height': None}
 
     def __init__(self, *args, **kwargs):
-        if 'width' not in kwargs and 'height' not in kwargs:
-            raise GgplotError(
-                "Specify either 'position' or 'width'/'height'")
+        print(kwargs)
+        if 'width' in kwargs or 'height' in kwargs:
+            if 'position' in kwargs:
+                raise GgplotError(
+                    "Specify either 'position' or 'width'/'height'")
 
-        with suppress(KeyError):
-            width = None
-            width = kwargs.pop('width')
+            with suppress(KeyError):
+                width = None
+                width = kwargs.pop('width')
 
-        with suppress(KeyError):
-            height = None
-            height = kwargs.pop('height')
+            with suppress(KeyError):
+                height = None
+                height = kwargs.pop('height')
 
-        kwargs['position'] = position_jitter(
-            width=width, height=height)
-        super(geom_jitter, self).__init__(*args, **kwargs)
+            kwargs['position'] = position_jitter(
+                width=width, height=height)
+        geom_point.__init__(self, *args, **kwargs)
