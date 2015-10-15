b593dcfa8c9c8e1506ffb2addb4ce6f95dce54e9
Hassan Kibirige <has2k1@gmail.com>
2015-10-14 21:40:09 -0500

    Fix and refactor stat_density
    - Some (those in the sandbox) statsmodel kde methods
      compute densities one point at a time.
    
    - Put the density calculation in compute_density.

--- ggplot/stats/stat_density.py
+++ ggplot/stats/stat_density.py
@@ -2,83 +2,105 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 import numpy as np
 import pandas as pd
-import pandas.core.common as com
 import statsmodels.api as sm
 
 from ..utils.exceptions import GgplotError
+from ..utils import suppress
 from .stat import stat
 
 
 # NOTE: Parameter discriptions are in
 # statsmodels/nonparametric/kde.py
-# TODO: Update when statsmodels-0.6 is released
 class stat_density(stat):
     REQUIRED_AES = {'x'}
     DEFAULT_PARAMS = {'geom': 'density', 'position': 'stack',
                       'kernel': 'gaussian', 'adjust': 1,
-                      'trim': False, 'n': 512, 'gridsize': None,
+                      'trim': False, 'n': 1024, 'gridsize': None,
                       'bw': 'normal_reference', 'cut': 3,
                       'clip': (-np.inf, np.inf)}
     DEFAULT_AES = {'y': '..density..'}
     CREATES = {'y'}
 
-    @classmethod
-    def compute_group(cls, data, scales, **params):
-        x = data.pop('x')
-        n = len(x)
-
-        range_x = scales.x.dimension((0, 0))
-
-        try:
-            float(x.iloc[0])
-        except:
-            try:
-                # try to use it as a pandas.tslib.Timestamp
-                x = [ts.toordinal() for ts in x]
-            except:
-                raise GgplotError(
-                    "stat_density(): aesthetic x mapping " +
-                    "needs to be convertable to float!")
-
-        # kde is computed efficiently using fft. But the fft does
-        # not support weights and is only available with the
-        # gaussian kernel. When weights are relevant we
-        # turn off the fft.
-        try:
-            weight = data.pop('weight')
-        except KeyError:
-            weight = np.ones(n) / n
-
+    def setup_params(self, data):
+        params = self.params.copy()
         lookup = {
             'biweight': 'biw',
             'cosine': 'cos',
+            'cosine2': 'cos2',
             'epanechnikov': 'epa',
             'gaussian': 'gau',
             'triangular': 'tri',
             'triweight': 'triw',
             'uniform': 'uni'}
-        kernel = lookup[params['kernel'].lower()]
 
-        if kernel == 'gaussian':
-            fft, weight = True, None
+        with suppress(KeyError):
+            params['kernel'] = lookup[params['kernel'].lower()]
+
+        if params['kernel'] not in lookup.viewvalues():
+            msg = ("kernel should be one of {}. "
+                   "You may use the abbreviations {}")
+            raise GgplotError(msg.format(lookup.viewkeys(),
+                                         lookup.viewvalues))
+
+        return params
+
+    @classmethod
+    def compute_group(cls, data, scales, **params):
+        weight = data.get('weight')
+
+        if params['trim']:
+            range_x = data['y'].min(), data['y'].max()
         else:
-            fft = False
+            range_x = scales.y.dimension()
 
-        kde = sm.nonparametric.KDEUnivariate(x)
-        kde.fit(
-            # kernel=kernel,        # enable after statsmodels 0.6
-            # bw=params['bw'], # enable after statsmodels 0.6
-            fft=fft,
-            weights=weight,
-            adjust=params['adjust'],
-            cut=params['cut'],
-            gridsize=params['gridsize'],
-            clip=params['clip'],)
-        x2 = np.linspace(range_x[0], range_x[1], params['n'])
+        return compute_density(data['x'], weight, range_x, **params)
+
+
+def compute_density(x, weight, range, **params):
+    n = len(x)
+
+    if weight is None:
+        weight = np.ones(n) / n
+
+    # if less than 3 points, spread density evenly
+    # over points
+    if n < 3:
+        return pd.DataFrame({
+            'x': x,
+            'density': weight / np.sum(weight),
+            'scaled': weight / np.max(weight),
+            'count': 1,
+            'n': n})
+
+    # kde is computed efficiently using fft. But the fft does
+    # not support weights and is only available with the
+    # gaussian kernel. When weights are relevant we
+    # turn off the fft.
+    if params['kernel'] == 'gau':
+        fft, weight = True, None
+    else:
+        fft = False
+
+    kde = sm.nonparametric.KDEUnivariate(x)
+    kde.fit(
+        kernel=params['kernel'],
+        bw=params['bw'],
+        fft=fft,
+        weights=weight,
+        adjust=params['adjust'],
+        cut=params['cut'],
+        gridsize=params['gridsize'],
+        clip=params['clip'])
+
+    x2 = np.linspace(range[0], range[1], params['n'])
+
+    try:
         y = kde.evaluate(x2)
-        new_data = pd.DataFrame({'x': x2,
-                                 'density': y,
-                                 'count': y * n,
-                                 'scaled': y / np.max(y),
-                                 'n': n})
-        return new_data
+    except ValueError:
+        y = np.array([kde.evaluate(_x)[0] for _x in x2])
+
+    return pd.DataFrame({'x': x2,
+                         'density': y,
+                         'scaled': y / np.max(y),
+                         'count': y * n,
+                         'n': n})
