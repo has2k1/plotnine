4a907426d43219869b5ea4a0f504162252aca33d
Hassan Kibirige <has2k1@gmail.com>
2016-07-14 22:07:12 -0500

    Add geom_qq tests

---
+++ ggplot/tests/test_geom_qq.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import pandas as pd
+
+from .. import ggplot, aes, geom_qq
+from .conftest import cleanup
+
+prng = np.random.RandomState(1234567890)
+
+
+@cleanup
+def test_normal():
+    df = pd.DataFrame({'x': prng.normal(size=100)})
+    p = ggplot(df, aes(sample='x')) + geom_qq()
+    # Roughly a straight line of points through the origin
+    assert p == 'normal'
