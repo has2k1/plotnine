97080ead8723ce8204dfb3f5e85ec93c6b09cab4
Hassan Kibirige <has2k1@gmail.com>
2017-04-03 22:53:16 -0500

    Add automatic theme compilation from doc directory
    Can now do
    
       make theme

---
+++ tools/build_theme.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# This script makes it easy to recompile install and tryout
+# a theme in case of any changes. It assumes the
+# sphinx-bootstrap-theme-customizer package is in the same directory
+# as the package, i.e one up the git toplevel.
+
+color5=$(tput setaf 5)
+reset=$(tput sgr0)      # reset color codes
+
+toplevel=$(git rev-parse --show-toplevel)
+
+# Compile theme
+cd $toplevel/../sphinx-bootstrap-theme-customizer/
+build "$@"
+
+# Copy theme for sphinx to use
+rm -rf $toplevel/doc/theme
+cp -rf theme $toplevel/doc
+
+# Do a hotswap (it may be enough for us to see the results compared to
+# regenerating the html)
+cd $toplevel
+cp $toplevel/doc/{theme/static,_build/html/_static}/bootstrap-3.3.6/css/bootstrap.min.css
+
+echo "${color5}success${reset}"
