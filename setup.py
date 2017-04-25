caba33343b14365e0c276b42aed9e14ff8ed50c9
Hassan Kibirige <has2k1@gmail.com>
2017-04-24 22:26:17 -0500

    Add long description to setup()

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+"""
+plotnine is an implementation of a *grammar of graphics* in Python,
+it is based on ggplot2. The grammar allows users to compose plots
+by explicitly mapping data to the visual objects that make up the
+plot.
+
+Plotting with a grammar is powerful, it makes custom (and otherwise
+complex) plots are easy to think about and then create, while the
+simple plots remain simple.
+
+To find out about all building blocks that you can use to create a
+plot, check out the documentation_. Since plotnine has an API
+similar to ggplot2, where we lack in coverage the
+`ggplot2 documentation`_ may be of some help.
+"""
 import os
 from setuptools import find_packages, setup
 import versioneer
