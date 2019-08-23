7b9eed7233601213559d0a70dbb0988d413aa384
Hassan Kibirige <has2k1@gmail.com>
2019-08-23 20:37:11 +0300

    Remove matplotlib Deprecated rcParam warnings
    Possibly due to MPL's slipup in the book keeping of the
    deprecated rcParams the changes in this commit could not
    catch them all.
    
    closes #308

--- plotnine/themes/theme_matplotlib.py
+++ plotnine/themes/theme_matplotlib.py
@@ -38,9 +38,13 @@ class theme_matplotlib(theme):
 
         if use_defaults:
             _copy = mpl.rcParams.copy()
+            deprecated_rcparams = (
+                set(mpl._deprecated_remain_as_none)
+                | set(mpl._all_deprecated)
+            )
             # no need to a get a deprecate warning just because
             # they are still included in rcParams...
-            for key in mpl._deprecated_map:
+            for key in deprecated_rcparams:
                 if key in _copy:
                     del _copy[key]
             if 'tk.pythoninspect' in _copy:
