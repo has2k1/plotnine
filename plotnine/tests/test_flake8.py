9c8f27b5db7630ff3cd44d35874d24692a128e4f
Hassan Kibirige <has2k1@gmail.com>
2020-06-01 18:07:14 +0300

    Remove unittest class, rely on pytest
    - Do not report stderr messages as they usually contain
    unnecessary warnings.

--- plotnine/tests/test_flake8.py
+++ plotnine/tests/test_flake8.py
@@ -1,20 +1,20 @@
+import os
 import subprocess
-import unittest
 from pathlib import Path
 
-file_dir = Path(__file__).parent
 
-
-class Flake8TestCase(unittest.TestCase):
-    def test_flake8(self):
+if not os.environ.get('TRAVIS'):
+    def test_flake8():
+        plotnine_dir = str(Path(__file__).parent.parent.absolute())
         p = subprocess.Popen(
-            ["flake8", str((file_dir.parent).absolute())],
+            ['flake8', plotnine_dir],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
-        stdout, stderr = p.communicate()
-        if p.returncode != 0:
-            self.fail(
-                "Flake 8 found issues: %s\n%s"
-                % (stdout.decode("utf-8"), stderr.decode("utf-8"))
-            )
+        # Ignore the stderr msg as it is mostly noise
+        # about coverage warnings
+        stdout, _ = p.communicate()
+        msg = "flake8 found the following issues: \n\n{}".format(
+                 stdout.decode('utf-8')
+        )
+        assert p.returncode == 0, msg
