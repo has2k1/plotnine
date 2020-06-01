c1a3c87722e1e4aa77124da2918887cd109b7fbf
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2020-06-01 17:21:57 +0200

    TST: On cmdline, run flake8 when pytesting (#305)
    * QoL: Flake8 test case
    
    Replaces the Travis based flake8 linting
    with a pytest test case. This eases the burden
    on contributers to remember running flake8 before
    submitting a PR and should speed up the travis runs a bit.
    
    * Revert lint test changes to .travis.yml
    
    * Remove unittest class, rely on pytest
    
    - Do not report stderr messages as they usually contain
    unnecessary warnings.
    
    Co-authored-by: Hassan Kibirige <has2k1@gmail.com>

---
+++ plotnine/tests/test_flake8.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+from pathlib import Path
+
+
+if not os.environ.get('TRAVIS'):
+    def test_flake8():
+        plotnine_dir = str(Path(__file__).parent.parent.absolute())
+        p = subprocess.Popen(
+            ['flake8', plotnine_dir],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        # Ignore the stderr msg as it is mostly noise
+        # about coverage warnings
+        stdout, _ = p.communicate()
+        msg = "flake8 found the following issues: \n\n{}".format(
+                 stdout.decode('utf-8')
+        )
+        assert p.returncode == 0, msg
