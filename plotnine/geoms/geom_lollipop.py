67a31c37ef7d42170850cddb5db06ad60182794f
Ben <ben.dilday.phd@gmail.com>
2020-10-10 22:34:49 -0400

    adds horizontal option to geom_lollipop

--- plotnine/geoms/geom_lollipop.py
+++ plotnine/geoms/geom_lollipop.py
@@ -23,7 +23,8 @@ class geom_lollipop(geom):
     NON_MISSING_AES = {'xend', 'yend'}
     DEFAULT_PARAMS = {**geom_segment.DEFAULT_PARAMS, **geom_point.DEFAULT_PARAMS,
                       'point_color': geom_point.DEFAULT_AES['color'],
-                      'point_size': geom_point.DEFAULT_AES['size']}
+                      'point_size': geom_point.DEFAULT_AES['size'],
+                      'horizontal': False}
     DEFAULT_AES = {**geom_segment.DEFAULT_AES, **geom_point.DEFAULT_AES}
 
     def setup_data(self, data):
@@ -34,5 +35,17 @@ class geom_lollipop(geom):
 
     @staticmethod
     def draw_group(data, panel_params, coord, ax, **params):
-        geom_segment.draw_group(data, panel_params, coord, ax, **params)
-        geom_point.draw_group(data.assign(color=params['point_color'], size=params['point_size']), panel_params, coord, ax, **params)
+
+        if params["horizontal"]:
+            plotting_data = data.copy()
+            plotting_data["yend"] = plotting_data["y"]
+            plotting_data["xend"] = 0
+        else:
+            plotting_data = data
+        geom_segment.draw_group(plotting_data, panel_params, coord, ax, **params)
+        geom_point.draw_group(
+            plotting_data.assign(
+                color=params['point_color'],
+                size=params['point_size']
+            ),
+            panel_params, coord, ax, **params)
