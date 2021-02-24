7f2c8d7655ab9d5737d71a76943555f196e76749
Florian Finkernagel <TyberiusPrime@users.noreply.github.com>
2021-02-24 11:54:46 +0100

    Fix: annotation_logticks + discrete axis + coord_flip & documentation (#492)

--- plotnine/geoms/annotation_logticks.py
+++ plotnine/geoms/annotation_logticks.py
@@ -46,7 +46,7 @@ class _geom_logticks(geom_rug):
             Sides onto which to draw the marks. Any combination
             chosen from the characters ``btlr``, for *bottom*, *top*,
             *left* or *right* side marks. If ``coord_flip()`` is used,
-            these are the sides *after* the flip.
+            these are the sides *before* the flip.
         panel_params : SimpleNamespace
             ``x`` and ``y`` view scale values.
         coord : coord
@@ -74,12 +74,12 @@ class _geom_logticks(geom_rug):
 
         if 't' in sides or 'b' in sides:
             if base_x is None:
-                if x_is_log:
+                if x_is_log and hasattr(x_scale, 'trans'):
                     base_x = x_scale.trans.base
                 else:  # no log, no defined base. See warning below.
                     base_x = 10
 
-            if not x_is_log:
+            if not hasattr(x_scale, 'trans') or not x_is_log:
                 warnings.warn(
                     "annotation_logticks for x-axis which does not have "
                     "a log scale. The logticks may not make sense.",
@@ -93,12 +93,12 @@ class _geom_logticks(geom_rug):
 
         if 'l' in sides or 'r' in sides:
             if base_y is None:
-                if y_is_log:
+                if y_is_log and hasattr(y_scale, 'trans'):
                     base_y = y_scale.trans.base
                 else:  # no log, no defined base. See warning below.
                     base_y = 10
 
-            if not y_is_log:
+            if not hasattr(y_scale, 'trans') or not y_is_log:
                 warnings.warn(
                     "annotation_logticks for y-axis which does not have "
                     "a log scale. The logticks may not make sense.",
