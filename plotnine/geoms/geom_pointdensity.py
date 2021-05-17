1cbe6eabf233e4fcd27440a9787be5eb63e63649
Josh Hartmann <joshhartmann11@gmail.com>
2021-05-17 03:08:56 -0700

    Add geom_pointdensity (#468)
    * Add geom_pointdensity
    
    * Fix geom_pointdensity documentation error
    
    * Add a count method for KDE

---
+++ plotnine/geoms/geom_pointdensity.py
@@ -0,0 +1,17 @@
+from ..doctools import document
+from .geom_point import geom_point
+
+
+@document
+class geom_pointdensity(geom_point):
+    """
+    Scatterplot with density estimation at each point
+
+    {usage}
+
+    Parameters
+    ----------
+    {common_parameters}
+    """
+    DEFAULT_PARAMS = {'stat': 'pointdensity', 'position': 'identity',
+                      'na_rm': False}
