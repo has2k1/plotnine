c7c8b103a7eedeb2d5166e881dedc9344a283fdd
Hassan Kibirige <has2k1@gmail.com>
2022-05-03 17:16:20 +0300

    MAINT: Use setuptools_scm, remove versioneer

--- setup.py
+++ setup.py
@@ -1,15 +1,4 @@
-import sys
 from setuptools import setup
 
-# This appears to be necessary so that versioneer works
-# Ref: python-versioneer/python-versioneer/issues/193
-sys.path.insert(0, ".")  # noqa
-import versioneer        # noqa
 
-
-setup(
-    # Move these to setup.cfg or pyproject.toml
-    # whenever it is possible to do so
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
-)
+setup()
