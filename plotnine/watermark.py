dba50d60c91cb5620cc31c69306f9dfa5538ce2a
Emerentius <emerentius@arcor.de>
2022-11-05 21:49:02 +0100

    type hint watermark

--- plotnine/watermark.py
+++ plotnine/watermark.py
@@ -1,4 +1,13 @@
+from __future__ import annotations
+
 import matplotlib.image as mimage
+
+import typing
+if typing.TYPE_CHECKING:
+    from typing import Any
+    import plotnine as p9
+    import pathlib
+    import matplotlib.figure
 
 
 class watermark:
@@ -24,21 +33,28 @@ class watermark:
     You can add more than one watermark to a plot.
     """
 
-    def __init__(self, filename, xo=0, yo=0, alpha=None, **kwargs):
+    def __init__(
+        self,
+        filename: str | pathlib.Path,
+        xo: int = 0,
+        yo: int = 0,
+        alpha: float | None = None,
+        **kwargs: Any
+    ):
         self.filename = filename
         kwargs.update(xo=xo, yo=yo, alpha=alpha)
         if 'zorder' not in kwargs:
             kwargs['zorder'] = 99.9
         self.kwargs = kwargs
 
-    def __radd__(self, gg):
+    def __radd__(self, gg: p9.ggplot) -> p9.ggplot:
         """
         Add watermark to ggplot object
         """
         gg.watermarks.append(self)
         return gg
 
-    def draw(self, figure):
+    def draw(self, figure: matplotlib.figure.Figure) -> None:
         """
         Draw watermark
 
