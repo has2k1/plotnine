0724b8ec94394a11d24cad7e31af5c6e30746224
Emerentius <Emerentius@arcor.de>
2022-11-08 09:00:35 +0100

    Add minimal typecheck infrastructure to CI (#644)
    * add mypy.ini, but ignore errors at first
    
    * type hint labels
    
    * type hint options, exceptions
    
    Also fix an import error in options.
    It attempted a relative import beyond the top level package.
    
    * Add typecheck job to CI
    
    * resolve feedback

--- plotnine/exceptions.py
+++ plotnine/exceptions.py
@@ -1,10 +1,13 @@
+from __future__ import annotations
 from textwrap import dedent
 import warnings
 
 
 # Show the warnings on one line, leaving out any code makes the
 # message clear
-def warning_format(message, category, filename, lineno, file=None, line=None):
+def warning_format(  # type: ignore
+    message, category, filename, lineno, line=None
+):
     """
     Format for plotnine warnings
     """
@@ -20,11 +23,11 @@ class PlotnineError(Exception):
     Exception for ggplot errors
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args: str) -> None:
         args = [dedent(arg) for arg in args]
         self.message = " ".join(args)
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Error Message
         """
