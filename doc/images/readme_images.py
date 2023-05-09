131a0f89f4b703a0cf51e196c0ca348c97b6442b
Hassan Kibirige <has2k1@gmail.com>
2023-05-09 15:38:30 +0300

    Regenerate readme & logo images
    Logo now uses Helvetica font.

--- doc/images/readme_images.py
+++ doc/images/readme_images.py
@@ -4,27 +4,30 @@ from plotnine import (
     geom_point,
     ggplot,
     stat_smooth,
+    theme,
     theme_tufte,
     theme_xkcd,
 )
 from plotnine.data import mtcars
 
-kwargs = {"width": 6, "height": 4, "dpi": 300}
-
-p1 = ggplot(mtcars, aes("wt", "mpg")) + geom_point()
-p1.save("readme-image-1.png", **kwargs)
+p1 = (
+    ggplot(mtcars, aes("wt", "mpg"))
+    + geom_point()
+    + theme(figure_size=(6, 4), dpi=300)
+)
+p1.save("readme-image-1.png")
 
 p2 = p1 + aes(color="factor(gear)")
-p2.save("readme-image-2.png", **kwargs)
+p2.save("readme-image-2.png")
 
 p3 = p2 + stat_smooth(method="lm")
-p3.save("readme-image-3.png", **kwargs)
+p3.save("readme-image-3.png")
 
 p4 = p3 + facet_wrap("~gear")
-p4.save("readme-image-4.png", **kwargs)
+p4.save("readme-image-4.png")
 
 p5 = p4 + theme_xkcd()
-p5.save("readme-image-5.png", **kwargs)
+p5.save("readme-image-5.png")
 
 p5alt = p4 + theme_tufte()
-p5alt.save("readme-image-5alt.png", **kwargs)
+p5alt.save("readme-image-5alt.png")
