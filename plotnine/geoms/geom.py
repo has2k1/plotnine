78ed06356aab3a6973915b44ec8ee9981be2f09d
Hassan Kibirige <has2k1@gmail.com>
2023-06-14 13:30:15 +0300

    LINT: Make ruff handle type-checking imports

--- plotnine/geoms/geom.py
+++ plotnine/geoms/geom.py
@@ -4,8 +4,6 @@ import typing
 from copy import deepcopy
 from itertools import chain, repeat
 
-import pandas as pd
-
 from ..exceptions import PlotnineError
 from ..layer import layer
 from ..mapping.aes import is_valid_aesthetic, rename_aesthetics
@@ -23,6 +21,8 @@ from ..utils import (
 
 if typing.TYPE_CHECKING:
     from typing import Any
+
+    import pandas as pd
 
     from plotnine.iapi import panel_view
     from plotnine.typing import (
