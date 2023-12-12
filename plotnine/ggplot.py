f97902d85129f17a52d6448bd7b1cfcceff56847
Hassan Kibirige <has2k1@gmail.com>
2023-12-12 13:01:54 +0300

    Remove ggplot.__[sg]etstate__
    The methods should not have been added.

--- plotnine/ggplot.py
+++ plotnine/ggplot.py
@@ -147,15 +147,6 @@ class ggplot:
 
         return result
 
-    def __getstate__(self):
-        d = dict(self.__dict__)
-        del d["environment"]
-        return d
-
-    def __setstate__(self, d):
-        d["environment"] = {}
-        return d
-
     def __iadd__(
         self, other: PlotAddable | list[PlotAddable] | None
     ) -> ggplot:
