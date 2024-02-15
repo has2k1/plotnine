6d1676ec522e8bc51de3f7a20788734a3bb9ca49
Hassan Kibirige <has2k1@gmail.com>
2024-02-15 22:14:21 +0300

    DOC: Put example notebooks in reference folder

--- doc/gallery/_gallery.py
+++ doc/gallery/_gallery.py
@@ -18,7 +18,7 @@ DOC_DIR = THIS_DIR.parent
 
 # String in code cell that creates an image that will be in the gallery
 GALLERY_TAG = "# Gallery Plot"
-EXAMPLES_DIR = DOC_DIR / "examples"
+EXAMPLES_DIR = DOC_DIR / "reference" / "examples"
 THUMBNAILS_DIR = Path("thumbnails")
 THUMBNAIL_SIZE = (294, 210)
 
