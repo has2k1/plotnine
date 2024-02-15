2df176265f4749dde0c0a8734c575e1c878c9481
Hassan Kibirige <has2k1@gmail.com>
2024-02-15 22:29:14 +0300

    Remove commented out print statement

--- plotnine/facets/facet_wrap.py
+++ plotnine/facets/facet_wrap.py
@@ -165,7 +165,6 @@ class facet_wrap(facet):
 
         # assign each point to a panel
         if len(facet_vals) and len(facet_vals.columns):
-            # print(type(facet_vals))
             keys = join_keys(facet_vals, layout, self.vars)
             data["PANEL"] = match(keys["x"], keys["y"], start=1)
         else:
