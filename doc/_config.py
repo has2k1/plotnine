a63cad37a9ce4fd5da83309dfab776e929864218
Hassan Kibirige <has2k1@gmail.com>
2024-02-27 19:03:19 +0300

    Remove all example and tutorials handling

--- doc/_config.py
+++ doc/_config.py
@@ -1,11 +1,8 @@
 """
 This script is run by the Makefile before quarto_build
 """
-import hashlib
 import re
-import shutil
 from importlib.metadata import version as get_version
-from importlib.resources import files as _files
 from pathlib import Path
 
 DOC_DIR = Path(__file__).parent
@@ -40,38 +37,5 @@ def generate_variables_file():
     variables_filepath.write_text(contents)
 
 
-def copy_examples_and_tutorials():
-    """
-    Copy the examples & tutorials in plotnine_examples
-    """
-
-    # NOTE: To avoid confusing the watcher used by "quarto preview",
-    # we copy only if the original files are different.
-    def same_contents(f1, f2):
-        h1 = hashlib.sha256(f1.read_bytes()).hexdigest()
-        h2 = hashlib.sha256(f2.read_bytes()).hexdigest()
-        return h1 == h2
-
-    def copy(src_dir, dest_dir):
-        dest_dir.mkdir(parents=True, exist_ok=True)
-        src_files = src_dir.glob("*.ipynb")
-        cur_dest_files = dest_dir.glob("*.ipynb")
-        new_dest_files = []
-        for src in src_files:
-            dest = dest_dir / src.name
-            new_dest_files.append(dest)
-            if dest.exists() and same_contents(src, dest):
-                continue
-            shutil.copyfile(src, dest)
-
-        # Remove any deleted files
-        for dest in set(cur_dest_files).difference(new_dest_files):
-            dest.unlink()
-
-    copy(_files("plotnine_examples.examples"), EXAMPLES_DIR)
-    copy(_files("plotnine_examples.tutorials"), TUTORIALS_DIR)
-
-
 if __name__ == "__main__":
     generate_variables_file()
-    copy_examples_and_tutorials()
