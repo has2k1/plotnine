3a7c5bb11a63d5baab930ac02bf3a531ab59adec
Hassan Kibirige <has2k1@gmail.com>
2024-06-12 20:09:39 +0300

    LINT: Long lines

--- plotnine/_mpl/_plotnine_tight_layout.py
+++ plotnine/_mpl/_plotnine_tight_layout.py
@@ -171,12 +171,16 @@ def set_figure_artist_positions(
     if pack.plot_caption:
         ha = theme.getp(("plot_caption", "ha"), "right")
         pack.plot_caption.set_y(sides.b.edge("plot_caption"))
-        horizontally_align_text_with_panels(pack.plot_caption, params, ha, pack)
+        horizontally_align_text_with_panels(
+            pack.plot_caption, params, ha, pack
+        )
 
     if pack.axis_title_x:
         ha = theme.getp(("axis_title_x", "ha"), "center")
         pack.axis_title_x.set_y(sides.b.edge("axis_title_x"))
-        horizontally_align_text_with_panels(pack.axis_title_x, params, ha, pack)
+        horizontally_align_text_with_panels(
+            pack.axis_title_x, params, ha, pack
+        )
 
     if pack.axis_title_y:
         va = theme.getp(("axis_title_y", "va"), "center")
