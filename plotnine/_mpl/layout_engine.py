e4ff6ad9b7f370d4a1280583c07af21f0058b564
Hassan Kibirige <has2k1@gmail.com>
2024-11-08 12:29:18 +0300

    Move LayoutPack to its own module

--- plotnine/_mpl/layout_engine.py
+++ plotnine/_mpl/layout_engine.py
@@ -1,45 +1,16 @@
 from __future__ import annotations
 
-import typing
-from dataclasses import asdict, dataclass
+from dataclasses import asdict
+from typing import TYPE_CHECKING
 
 from matplotlib.layout_engine import LayoutEngine
-from matplotlib.text import Text
 
-if typing.TYPE_CHECKING:
-    from typing import Any, Optional
+from ._layout_pack import LayoutPack
 
-    from matplotlib.axes import Axes
-    from matplotlib.backend_bases import RendererBase
+if TYPE_CHECKING:
     from matplotlib.figure import Figure
 
-    from plotnine import ggplot, theme
-    from plotnine._mpl.text import StripText
-    from plotnine.facets.facet import facet
-    from plotnine.iapi import legend_artists
-
-
-@dataclass
-class LayoutPack:
-    """
-    Objects required to compute the layout
-    """
-
-    axs: list[Axes]
-    figure: Figure
-    renderer: RendererBase
-    theme: theme
-    facet: facet
-    axis_title_x: Optional[Text] = None
-    axis_title_y: Optional[Text] = None
-    # The legends references the structure that contains the
-    # AnchoredOffsetboxes (groups of legends)
-    legends: Optional[legend_artists] = None
-    plot_caption: Optional[Text] = None
-    plot_subtitle: Optional[Text] = None
-    plot_title: Optional[Text] = None
-    strip_text_x: Optional[list[StripText]] = None
-    strip_text_y: Optional[list[StripText]] = None
+    from plotnine import ggplot
 
 
 class PlotnineLayoutEngine(LayoutEngine):
@@ -67,44 +38,10 @@ class PlotnineLayoutEngine(LayoutEngine):
             set_figure_artist_positions,
         )
 
-        pack = self.setup()
+        pack = LayoutPack(self.plot)
 
         with getattr(pack.renderer, "_draw_disabled", nullcontext)():
             tparams = get_plotnine_tight_layout(pack)
 
         fig.subplots_adjust(**asdict(tparams.params))
         set_figure_artist_positions(pack, tparams)
-
-    def setup(self) -> LayoutPack:
-        """
-        Put together objects required to do the layout
-        """
-        targets = self.theme.targets
-
-        def get_target(name: str) -> Any:
-            """
-            Return themeable target or None
-            """
-            if self.theme.T.is_blank(name):
-                return None
-            else:
-                t = getattr(targets, name)
-                if isinstance(t, Text) and t.get_text() == "":
-                    return None
-                return t
-
-        return LayoutPack(
-            axs=self.plot.axs,
-            figure=self.plot.figure,
-            renderer=self.plot.figure._get_renderer(),  # pyright: ignore
-            theme=self.theme,
-            facet=self.plot.facet,
-            axis_title_x=get_target("axis_title_x"),
-            axis_title_y=get_target("axis_title_y"),
-            legends=get_target("legends"),
-            plot_caption=get_target("plot_caption"),
-            plot_subtitle=get_target("plot_subtitle"),
-            plot_title=get_target("plot_title"),
-            strip_text_x=get_target("strip_text_x"),
-            strip_text_y=get_target("strip_text_y"),
-        )
