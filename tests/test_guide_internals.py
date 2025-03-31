b6682c2e8406fc653b15fa2ecbe9d01c79a95e35
Hassan Kibirige <has2k1@gmail.com>
2025-03-31 17:51:41 +0300

    Fix after_scale aesthetic to show up in the legend
    fixes #926

--- tests/test_guide_internals.py
+++ tests/test_guide_internals.py
@@ -1,7 +1,11 @@
 import warnings
 
+import pandas as pd
+
 from plotnine import (
     aes,
+    after_scale,
+    geom_bar,
     geom_point,
     ggplot,
 )
@@ -14,3 +18,26 @@ def test_no_after_scale_warning():
     with warnings.catch_warnings():
         warnings.simplefilter("error")
         p.draw_test()  # type: ignore
+
+
+def test_guide_legend_after_scale():
+    def alphen(series, a):
+        ha = f"{round(a * 255):#04X}"[2:]
+        return [f"{hex_color}{ha}" for hex_color in series]
+
+    data = pd.DataFrame(
+        {"var1": [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5]}
+    )
+    p = (
+        ggplot(
+            data,
+            aes(
+                "var1",
+                color="factor(var1)",
+                fill=after_scale("alphen(color, .5)"),
+            ),
+        )
+        + geom_bar()
+    )
+
+    assert p == "guide_legend_after_scale"
