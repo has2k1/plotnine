09f87a660d71c71ec89821bef1a919ae2d9a67a8
Hassan Kibirige <has2k1@gmail.com>
2025-07-03 23:42:57 +0300

    Rename spacer to plot_spacer

--- plotnine/composition/__init__.py
+++ plotnine/composition/__init__.py
@@ -1,11 +1,11 @@
 from ._arrange import Arrange
 from ._beside import Beside
-from ._spacer import spacer
+from ._plot_spacer import plot_spacer
 from ._stack import Stack
 
 __all__ = (
     "Arrange",
     "Stack",
     "Beside",
-    "spacer",
+    "plot_spacer",
 )
---
+++ plotnine/composition/_plot_spacer.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from copy import deepcopy
+
+from plotnine import element_rect, ggplot, theme, theme_void
+
+
+class plot_spacer(ggplot):
+    """
+    Blank area as wide or as tall as a plot
+
+    Parameters
+    ----------
+    fill :
+        Background color. The default is a transparent area, but it
+        can be changed through this parameter.
+
+        The color can also be modified by adding a [](`~plotnine.theme`)
+        and setting the [](`~plotnine.themes.themeable.plot_background`).
+    """
+
+    def __init__(
+        self,
+        fill: (
+            str
+            | tuple[float, float, float]
+            | tuple[float, float, float, float]
+            | None
+        ) = None,
+    ):
+        super().__init__()
+        self.theme = theme_void()
+        if fill:
+            self.theme += theme(plot_background=element_rect(fill=fill))
+
+    def __add__(self, rhs) -> plot_spacer:
+        """
+        Add to spacer
+
+        All added objects are no ops except the `plot_background` in
+        in a theme.
+        """
+        self = deepcopy(self)
+        if isinstance(rhs, theme):
+            fill = rhs.getp(("plot_background", "facecolor"))
+            self.theme += theme(
+                plot_background=element_rect(fill=fill),
+            )
+        return self
