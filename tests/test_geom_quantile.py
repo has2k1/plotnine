50ecbf5a57bd50ff48db36a924483f329f577f2c
Hassan Kibirige <has2k1@gmail.com>
2025-07-31 00:49:12 +0300

    Fix stat_quantile to handle formula that use environment variables

--- tests/test_geom_quantile.py
+++ tests/test_geom_quantile.py
@@ -18,7 +18,18 @@ def test_lines():
         + geom_quantile(quantiles=[0.001, 0.5, 0.999], formula="y~x", size=2)
     )
 
+    # np.absolute tests the ability to pickup variables in the
+    # caller environment
+    p2 = (
+        ggplot(data, aes(x="x", y="y"))
+        + geom_point(alpha=0.5)
+        + geom_quantile(
+            quantiles=[0.001, 0.5, 0.999], formula="y~np.absolute(x)", size=2
+        )
+    )
+
     # Two (.001, .999) quantile lines should bound the points
     # from below and from above, and the .5 line should go
     # through middle (approximately).
     assert p == "lines"
+    assert p2 == "lines"
