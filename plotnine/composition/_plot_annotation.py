f70313705d42c114c7429b9ba5ed0ec10b1e1a5e
Hassan Kibirige <has2k1@gmail.com>
2025-09-26 15:52:11 -0700

    Add plot_annotation class

---
+++ plotnine/composition/_plot_annotation.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from copy import copy
+from dataclasses import dataclass, field
+from typing import TYPE_CHECKING
+
+from plotnine.themes.targets import ThemeTargets
+
+from .. import theme
+from .._utils.dataclasses import non_none_init_items
+from ..composition._types import ComposeAddable
+
+if TYPE_CHECKING:
+    from ._compose import Compose
+
+
+@dataclass(kw_only=True)
+class plot_annotation(ComposeAddable):
+    """
+    Annotate a composition
+    """
+
+    title: str | None = None
+    """
+    The title of the composition
+    """
+
+    subtitle: str | None = None
+    """
+    The subtitle of the composition
+    """
+
+    caption: str | None = None
+    """
+    The caption of the composition
+    """
+
+    theme: theme = field(default_factory=theme)  # pyright: ignore[reportUnboundVariable]
+    """
+    Theme to use for the plot title, subtitle, caption, margin and background
+    """
+
+    def __radd__(self, cmp: Compose) -> Compose:
+        """
+        Add plot annotation to composition
+        """
+        cmp.annotation = self
+        return cmp
+
+    def update(self, other: plot_annotation):
+        """
+        Update this annotation with the contents of other
+        """
+        for name, value in non_none_init_items(other):
+            if name == "theme":
+                self.theme = self.theme + value
+            else:
+                setattr(self, name, value)
+
+    def _setup(self, cmp: Compose):
+        """
+        Setup annotation
+        """
+        # We mimick theme.setup instead of call it because
+        # we cannot have have a ggplot object.
+        self.theme = copy(self.theme)
+        self.theme.figure = cmp.figure
+        self.theme.targets = ThemeTargets()
+        self.theme._add_default_themeable_properties()
+        self.theme.T.setup(self.theme)
+
+    def empty(self) -> bool:
+        """
+        Whether the annotation has any content
+        """
+        for name, value in non_none_init_items(self):
+            if name == "theme":
+                return len(value.themeables) == 0
+            else:
+                return False
+
+        return True
+
+    def draw(self):
+        """
+        Render the items in the annotation
+        """
+        if self.empty():
+            return
+
+        figure = self.theme.figure
+        targets = self.theme.targets
+
+        if self.title:
+            targets.plot_title = figure.text(0, 0, self.title)
+
+        if subtitle := self.subtitle:
+            targets.plot_subtitle = figure.text(0, 0, subtitle)
+
+        if caption := self.caption:
+            targets.plot_caption = figure.text(0, 0, caption)
